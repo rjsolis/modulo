99f8a682b72059b88870127957611c6673ffe9db
rjsolis <63673765+rjsolis@users.noreply.github.com>
2020-05-13 14:04:29 -0500

    E3 A7
    E3 A7

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,11 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 
+
+interface Country {
+  name: string;
+  flag: string;
+  area: number;
+  population: number;
+}
+
+const COUNTRIES: Country[] = [
+  {
+    name: 'Mexico',
+    flag: 'f/fc/Flag_of_Mexico.svg',
+    area: 1964375,
+    population: 129163276
+  },
+  {
+    name: 'Canada',
+    flag: 'c/cf/Flag_of_Canada.svg',
+    area: 9976140,
+    population: 36624199
+  },
+  {
+    name: 'United States',
+    flag: 'a/a4/Flag_of_the_United_States.svg',
+    area: 9629091,
+    population: 324459463
+  },
+  {
+    name: 'China',
+    flag: 'f/fa/Flag_of_the_People%27s_Republic_of_China.svg',
+    area: 9596960,
+    population: 1409517397
+  }
+];
+
+
+
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css']
 })
 export class TableComponent implements OnInit {
+  countries = COUNTRIES;
 
   constructor() { }
 
